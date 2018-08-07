6582fd5401e37dad936102763a1d894a2b2a6b9b
Brian D. Weitzner <brian.weitzner@gmail.com>
2018-08-07 12:02:39 -0700

    Updating the text on the talk map

---
+++ talk_map/org-locations.js
@@ -0,0 +1,67 @@
+var addressPoints = [
+  [
+    "Huntington Beach, California | IBC Antibody Engineering and Therapeutics | Computational structure prediction, docking and design of antibodies",
+    33.6783336,
+    -118.0000166
+  ],
+  [
+    "San Diego, California | America's Antibody Congress | Computer-guided prediction and validation of antibody structures",
+    32.7174209,
+    -117.1627714
+  ],
+  [
+    "Austin, Texas | Institute for Cellular and Molecular Biology, University of Texas at Austin | Next-generation antibody modeling",
+    30.2711286,
+    -97.7436995
+  ],
+  [
+    "San Francisco, California | AIChE Annual Conference | Kinked CDR H3-like loops are common",
+    8.6545394,
+    -73.3676149
+  ],
+  [
+    "Leavenworth, Washington | Rosetta Conference | Using PyRosetta for research",
+    47.5969138,
+    -120.6610816
+  ],
+  [
+    "Leavenworth, Washington | Rosetta Conference | PyRosetta 2.0: I can make a new score term in 6 lines!",
+    47.5969138,
+    -120.6610816
+  ],
+  [
+    "Leavenworth, Washington | Rosetta Conference | Antibodies are proteins too!",
+    47.5969138,
+    -120.6610816
+  ],
+  [
+    "Huntington Beach, California | IBC Antibody Engineering and Therapeutics | Benchmarking RosettaAntibody: Antibody Modeling Assessment II",
+    33.6783336,
+    -118.0000166
+  ],
+  [
+    "Missoula, Montana  | Center for Biomolecular Structure and Dynamics, University of Montana | Next-generation antibody modeling",
+    46.8700801,
+    -113.9952796
+  ],
+  [
+    "Munich, Germany | Fakulta\u0308t fu\u0308r Chemie, Technische Universita\u0308t Mu\u0308nchen | The origin of CDR H3 structural diversity",
+    48.1371079,
+    11.5753822
+  ],
+  [
+    "San Diego, California | America\u2019s Antibody Congress | Producing physically realistic structural models with RosettaAntibody",
+    32.7174209,
+    -117.1627714
+  ],
+  [
+    "Leavenworth, Washington | Rosetta Conference | PyMOL\u2013PyRosetta Integration",
+    47.5969138,
+    -120.6610816
+  ],
+  [
+    "Bellevue, Washington | Institute for Disease Modeling | Best practices in collaborative software development: lessons learned in the Rosetta Commons",
+    47.6144219,
+    -122.1923372
+  ]
+];
